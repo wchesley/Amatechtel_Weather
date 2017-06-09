c2dbc4ac343f62cb4573ec76fb563790696cc46d
Walker <chesley.walker@gmail.com>
2017-06-08 20:03:14 -0500

    Added desired variables to console output.

--- AmaTechWeather_POC/Program.cs
+++ AmaTechWeather_POC/Program.cs
@@ -30,8 +30,13 @@ namespace AmaTechWeather_POC
                     if (json_data != null)
                     {
                         JsonResponse.RootObject token = JsonConvert.DeserializeObject<JsonResponse.RootObject>(json_data);
-                        var weather = token.current_observation.temperature_string;
-                        Console.WriteLine($"Temperature: {weather}");
+                        var temperature = token.current_observation.temperature_string;
+                        var wind = token.current_observation.wind_string;
+                        var rain = token.current_observation.precip_today_string;
+                        var heat_Index = token.current_observation.heat_index_string;
+                        var weather = token.current_observation.weather; 
+                        Console.WriteLine($"Temperature: {temperature}\nWind: {wind}\nRain: {rain}\nHeat Index: {heat_Index}\nFeels Like: {weather}");
+                        Console.ReadKey();
                     }
                     else
                     {
